9b1665a07478c9a17998b0dea19561029e5eeadb
lotfi-cheikh <lotfi.cheikh@laplateforme.iogit config --global user.email lotfi.cheikh@laplateforme.io>
2026-05-05 17:44:58 +0200

    chargement du mario

--- main.c
+++ main.c
@@ -10,8 +10,7 @@
 
 int init(SDL_Window **window, SDL_Renderer **renderer, int w, int h);
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
     if ( SDL_Init ( SDL_INIT_VIDEO ) < 0)
 {
         // gestion de l ’ erreur avec SDL
@@ -21,16 +20,52 @@ int main(int argc, char *argv[])
     // creation d ’ un renderer et d ’ une fenetre
     SDL_Window * window = NULL ;
     SDL_Renderer * renderer = NULL ;
-    if (0 != SDL_CreateWindowAndRenderer(500 , 500 , SDL_WINDOW_SHOWN, &window , &renderer ))
+    if (0 != SDL_CreateWindowAndRenderer(1000 , 1000 , SDL_WINDOW_SHOWN, &window , &renderer ))
 {
 
         fprintf (stderr , " Erreur ␣ SDL_CreateWindowAndRenderer␣:␣ %s " ,
             SDL_GetError ());
         return -1;
+    
+    
 }
 
+        SDL_Texture *textureMenu = loadImage("img/menu.jpg" ,renderer);
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer,textureMenu,NULL,NULL);
+        SDL_RenderPresent(renderer);
 
-SDL_Delay(2000);
+
+    int continuer = 1;
+    SDL_Event events;
+
+    while(continuer){ //coeur du jeu ici, les actions seront repété pour faire le déplacement des différentes images, ...
+        while (SDL_PollEvent(&events))
+        {
+            switch (events.type)
+            {
+            case SDL_QUIT:
+                continuer = 0;
+                break;
+            case SDL_KEYDOWN : // on a appuye sur une touche
+                switch ( events.key.keysym.sym) // liste des touches a p p u y
+                {
+                    case SDLK_1 :
+                        jouer(renderer);
+                        break;
+                        // on a appuye sur la touche 1
+                    case SDLK_2 :
+                        SDL_RenderClear(renderer);
+                        SDL_Texture *textureMenu = loadImage("img/goomba1.png" ,renderer);
+                        SDL_RenderCopy(renderer,textureMenu,NULL,NULL);
+                        SDL_RenderPresent(renderer);
+                        break;
+
+                }
+				
+            }
+        }
+    }
     return 0;
 }
 
